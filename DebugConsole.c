5d3d2040c8248187f45aa3645b4b9de644a876f5
Rossi Kwak <rossi.kwak@gmail.com>
2020-09-03 16:36:35 +0900

    modify log

--- DebugConsole.c
+++ DebugConsole.c
@@ -26,11 +26,11 @@ void CheckStatus(char* StringPtr, CyU3PReturnStatus_t Status)
 {
 	if (DebugTxEnabled)				// Need to wait until ConsoleOut is enabled
 	{
-		if (Status == CY_U3P_SUCCESS) CyU3PDebugPrint(4, "\r\n%s Successful\n", StringPtr);
+		if (Status == CY_U3P_SUCCESS) CyU3PDebugPrint(4, "%s OK\n", StringPtr);
 		else
 		{
 			// else hang here
-			CyU3PDebugPrint(4, "\r\n%s failed, Status = %d\r\n", StringPtr, Status);
+			CyU3PDebugPrint(4, "\r\n%s failed, Status = %d\n", StringPtr, Status);
 			while (1)
 			{
 				CyU3PDebugPrint(4, "?");
