a86a41fd6742320fe65be44089f31668b591d465
Rossi Kwak <rossi.kwak@gmail.com>
2020-09-07 13:45:03 +0900

    modify into standard header style, remove 'extern'

--- USB_Handler.h
+++ USB_Handler.h
@@ -1,30 +1,11 @@
+#ifndef __USB_HANDLER_H__
+#define __USB_HANDLER_H__
 
-#ifndef _USB_HANDLER_
-#define _USB_HANDLER_
+CyU3PReturnStatus_t USBEP0RxThread_Create(void);
+CyBool_t USBSetupCB(uint32_t setupdat0, uint32_t setupdat1);
+void USBEventCB(CyU3PUsbEventType_t evtype, uint16_t evdata);
+CyBool_t USBLPMRqtCB(CyU3PUsbLinkPowerMode link_mode);
+CyU3PReturnStatus_t USB_Init(void);
+CyU3PReturnStatus_t USB_Connect(void);
 
-extern CyU3PReturnStatus_t USBEP0RxThread_Create(void);
-extern CyBool_t
-USBSetupCB (
-        uint32_t setupdat0, /* SETUP Data 0 */
-        uint32_t setupdat1  /* SETUP Data 1 */
-    );
-extern void
-USBEventCB (
-    CyU3PUsbEventType_t evtype, /* Event type */
-    uint16_t            evdata  /* Event data */
-    );
-extern CyBool_t
-USBLPMRqtCB (
-        CyU3PUsbLinkPowerMode link_mode);
-extern CyU3PReturnStatus_t USB_Init(void);
-
-extern CyU3PReturnStatus_t USB_Connect(void);
-
-
-
-
-
-
-
-
-#endif   /* _INCLUDED__ */
+#endif
