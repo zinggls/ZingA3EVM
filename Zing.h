440e16025e20f9efa17b26be0809555bb5a22c79
Rossi Kwak <rossi.kwak@gmail.com>
2020-09-08 14:16:40 +0900

    remove needless extern keyword and rearrange coding style

--- Zing.h
+++ Zing.h
@@ -1,31 +1,22 @@
-#ifndef _ZING_
-#define _ZING_
+#ifndef __ZING_H__
+#define __ZING_H__
 
 #define ZING_RF_SERDES_PATH		(1)		//0 : serdes path, 1: rf path
 
-extern CyU3PReturnStatus_t Zing_Init(void);
-extern CyU3PReturnStatus_t Zing_RegWrite(uint16_t addr, uint8_t* buf, uint16_t len);
-extern CyU3PReturnStatus_t Zing_RegRead(uint16_t addr, uint8_t* buf, uint16_t len);
-extern void Zing_AFC2(float f_tg);
-extern void Zing_SetHRCP(uint32_t val);
-extern void Zing_SetPath(uint32_t val);
-extern CyU3PReturnStatus_t Zing_SendMsg(uint8_t* buf, uint32_t len);
-extern CyU3PReturnStatus_t Zing_RecvMsg(uint8_t* buf, uint32_t* len_pt);
-extern void Zing_Reset(uint8_t type);
-extern CyU3PReturnStatus_t Zing_AutoHRCP(void);
-extern uint32_t Zing_GetVersion(void);
+CyU3PReturnStatus_t Zing_Init(void);
+CyU3PReturnStatus_t Zing_RegWrite(uint16_t addr, uint8_t* buf, uint16_t len);
+CyU3PReturnStatus_t Zing_RegRead(uint16_t addr, uint8_t* buf, uint16_t len);
+void Zing_AFC2(float f_tg);
+void Zing_SetHRCP(uint32_t val);
+void Zing_SetPath(uint32_t val);
+CyU3PReturnStatus_t Zing_SendMsg(uint8_t* buf, uint32_t len);
+CyU3PReturnStatus_t Zing_RecvMsg(uint8_t* buf, uint32_t* len_pt);
+void Zing_Reset(uint8_t type);
+CyU3PReturnStatus_t Zing_AutoHRCP(void);
+uint32_t Zing_GetVersion(void);
+void Zing_Test_DataTx2 (uint32_t repeat, uint32_t length, uint32_t pattern);
+void Zing_Test_DataSink2 (uint32_t cnt, uint32_t timeout);
+CyU3PReturnStatus_t Zing_Transfer_Recv3(CyU3PDmaChannel *dma_ch,uint8_t *data,uint32_t *length_pt);
+CyU3PReturnStatus_t Zing_Management_Send(uint8_t *data,uint32_t length);
 
-extern void Zing_Test_DataTx2 (uint32_t repeat, uint32_t length, uint32_t pattern);
-extern void Zing_Test_DataSink2 (uint32_t cnt, uint32_t timeout);
-
-extern CyU3PReturnStatus_t Zing_Transfer_Recv3 (
-		CyU3PDmaChannel* dma_ch,
-	    uint8_t     *data,		/* pointer to msg */
-	    uint32_t*    length_pt	 	/* msg size in bytes */
-	    );
-
-extern CyU3PReturnStatus_t Zing_Management_Send (
-	    uint8_t     *data,		/* pointer to msg */
-	    uint32_t    length	 	/* msg size in bytes */
-	    );
-#endif   /* _INCLUDED__ */
+#endif
