a17369301cebaf6f8c6585cacae7ddfbb41d6705
Rossi Kwak <rossi.kwak@gmail.com>
2020-09-09 11:19:18 +0900

    remove comments

--- usb/USB_Descriptors.c
+++ usb/USB_Descriptors.c
@@ -1,25 +1,3 @@
-/*
- ## Cypress USB 3.0 Platform header file (cyfxbulklpdscr.c)
- ## ===========================
- ##
- ##  Copyright Cypress Semiconductor Corporation, 2010-2011,
- ##  All Rights Reserved
- ##  UNPUBLISHED, LICENSED SOFTWARE.
- ##
- ##  CONFIDENTIAL AND PROPRIETARY INFORMATION
- ##  WHICH IS THE PROPERTY OF CYPRESS.
- ##
- ##  Use of this file is governed
- ##  by the license agreement included in the file
- ##
- ##     <install>/license/license.txt
- ##
- ##  where <install> is the Cypress software
- ##  installation root directory path.
- ##
- ## ===========================
-*/
-
 /* This file contains the USB enumeration descriptors for the bulk loop application example.
  * The descriptor arrays must be 32 byte aligned and multiple of 32 bytes if the D-cache is
  * turned on. If the linker used is not capable of supporting the aligned feature for this,
