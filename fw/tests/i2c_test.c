2b69faa475f9b93fb39dbc232debdcb24b6708c3
Rossi Kwak <rossi.kwak@gmail.com>
2020-09-11 13:49:44 +0900

    1st init must successful but 2nd must fail

--- fw/tests/i2c_test.c
+++ fw/tests/i2c_test.c
@@ -4,5 +4,8 @@
 void test_i2c_init()
 {
 	CyU3PReturnStatus_t Status = I2C_Init();
-	assert("I2C Init",Status==CY_U3P_SUCCESS);
+	assert("1st I2C Init",Status==CY_U3P_SUCCESS);
+
+	Status = I2C_Init();
+	assert("2nd I2C Init",Status!=CY_U3P_SUCCESS);
 }
