6adc2b4f800e6c8ef745b84c04e859e3c9f9344c
Rossi Kwak <rossi.kwak@gmail.com>
2020-09-15 17:10:39 +0900

    move testing implementations into headers

--- fw/tests/pib_test.h
+++ fw/tests/pib_test.h
@@ -2,7 +2,12 @@
 #define __PIB_TEST_H__
 
 #include "Application.h"
+#include "gpif/PIB.h"
 
-void test_pib_init();
+void test_pib_init()
+{
+	assert("1st PIB Init",PIB_Init()==CY_U3P_SUCCESS);
+	assert("2nd PIB Init",PIB_Init()!=CY_U3P_SUCCESS);
+}
 
 #endif
