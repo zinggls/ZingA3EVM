ef21a971cf148810a5f63a96288e95c02a31408a
Rossi Kwak <rossi.kwak@gmail.com>
2020-09-16 13:34:56 +0900

    2nd zing init test added

--- fw/tests/zing_test.h
+++ fw/tests/zing_test.h
@@ -12,7 +12,8 @@ void test_zing_init()
 	DMA_Sync_mode(CY_FX_EP_CONTROL_IN,CY_FX_EP_CONTROL_OUT,CY_FX_EP_DATA_IN,CY_FX_EP_DATA_OUT,CY_FX_DATA_BURST_LENGTH);
 
 	assert("Control Channel Thread",ControlChThread_Create()==CY_U3P_SUCCESS);
-	assert("Zing Init",Zing_Init()==CY_U3P_SUCCESS);
+	assert("1st Zing Init",Zing_Init()==CY_U3P_SUCCESS);
+	assert("2nd Zing Init",Zing_Init()==CY_U3P_SUCCESS);
 }
 
 #endif
