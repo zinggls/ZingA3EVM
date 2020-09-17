7236d101a9d8f24f391172a44c5c09beca8f2c11
Rossi Kwak <rossi.kwak@gmail.com>
2020-09-17 14:04:25 +0900

    Apply macro

--- src/gpio.c
+++ src/gpio.c
@@ -1,10 +1,10 @@
 #include "gpio.h"
 #include "cyu3gpio.h"
 #include "cyu3error.h"
+#include "DebugConsole.h"
 
 CyU3PReturnStatus_t SetupGPIO(void)
 {
-	CyU3PReturnStatus_t Status;
 	CyU3PGpioClock_t GpioClock;
 	CyU3PGpioSimpleConfig_t GpioConfig;
 
@@ -14,33 +14,27 @@ CyU3PReturnStatus_t SetupGPIO(void)
 	GpioClock.simpleDiv = CY_U3P_GPIO_SIMPLE_DIV_BY_2;
 	GpioClock.clkSrc = CY_U3P_SYS_CLK;
 	GpioClock.halfDiv = 0;
-	Status = CyU3PGpioInit(&GpioClock, 0);
-	if(Status!=CY_U3P_SUCCESS) return Status;
+	CHECK(CyU3PGpioInit(&GpioClock, 0));
 
 	// GPIO57 - TP2
-	Status = CyU3PDeviceGpioOverride(GPIO57, CyTrue);
-	if(Status!=CY_U3P_SUCCESS) return Status;
+	CHECK(CyU3PDeviceGpioOverride(GPIO57, CyTrue));
 
 	CyU3PMemSet((uint8_t *)&GpioConfig, 0, sizeof(GpioConfig));
 	GpioConfig.outValue = CyFalse;
 	GpioConfig.driveLowEn = CyTrue;
 	GpioConfig.driveHighEn = CyTrue;
-	Status = CyU3PGpioSetSimpleConfig(GPIO57, &GpioConfig);
-	if(Status!=CY_U3P_SUCCESS) return Status;
+	CHECK(CyU3PGpioSetSimpleConfig(GPIO57, &GpioConfig));
 
 	// GPIF Bus Width
-	Status = CyU3PDeviceGpioOverride(GPIF_BUSWIDTH_CTL0, CyTrue);
-	if(Status!=CY_U3P_SUCCESS) return Status;
+	CHECK(CyU3PDeviceGpioOverride(GPIF_BUSWIDTH_CTL0, CyTrue));
 
 	CyU3PMemSet((uint8_t *)&GpioConfig, 0, sizeof(GpioConfig));
 	GpioConfig.outValue = CyFalse;
 	GpioConfig.driveLowEn = CyTrue;
 	GpioConfig.driveHighEn = CyTrue;
-	Status = CyU3PGpioSetSimpleConfig(GPIF_BUSWIDTH_CTL0, &GpioConfig);
-	if(Status!=CY_U3P_SUCCESS) return Status;
+	CHECK(CyU3PGpioSetSimpleConfig(GPIF_BUSWIDTH_CTL0, &GpioConfig));
 
-	Status = CyU3PDeviceGpioOverride(GPIF_BUSWIDTH_CTL1, CyTrue);
-	if(Status!=CY_U3P_SUCCESS) return Status;
+	CHECK(CyU3PDeviceGpioOverride(GPIF_BUSWIDTH_CTL1, CyTrue));
 
 	CyU3PMemSet((uint8_t *)&GpioConfig, 0, sizeof(GpioConfig));
 	GpioConfig.outValue = CyFalse;
