b5c9ea45c74c814bb7b93fb8354ebeb991f8a222
Rossi Kwak <rossi.kwak@gmail.com>
2020-10-30 10:03:25 +0900

    copy basic structues on otg from 'fx3/usbhost' example

--- fw/a3/otg.c
+++ fw/a3/otg.c
@@ -1,7 +1,128 @@
-#include "otg.h"
+#include "Application.h"
 #include "cyu3error.h"
 #include "cyu3system.h"
 #include "cyu3usb.h"
+#include "cyu3usbhost.h"
+#include "cyu3gpio.h"
+#include "cyu3utils.h"
+
+CyBool_t glIsApplnActive = CyFalse;					/* Whether the application is active or not. */
+volatile CyBool_t glIsPeripheralPresent = CyFalse;	/* Whether a remote peripheral is present or not. */
+
+/* Setup packet buffer for host mode. */
+uint8_t glSetupPkt[CY_FX_HOST_EP0_SETUP_SIZE] __attribute__ ((aligned (32)));
+
+/* USB host stack event callback function. */
+void CyFxHostEventCb (CyU3PUsbHostEventType_t evType, uint32_t evData)
+{
+    /* This is connect / disconnect event. Log it so that the
+     * application thread can handle it. */
+    if (evType == CY_U3P_USB_HOST_EVENT_CONNECT)
+    {
+        CyU3PDebugPrint (4, "Host connect event received\r\n");
+        glIsPeripheralPresent = CyTrue;
+    }
+    else
+    {
+        CyU3PDebugPrint (4, "Host disconnect event received\r\n");
+        glIsPeripheralPresent = CyFalse;
+    }
+
+    /* Signal the thread that a peripheral change has been detected. */
+    CyU3PEventSet (&applnEvent, CY_FX_USB_CHANGE_EVENT, CYU3P_EVENT_OR);
+}
+
+/* Helper function to send EP0 setup packet. */
+CyU3PReturnStatus_t
+CyFxSendSetupRqt (
+        uint8_t type,
+        uint8_t request,
+        uint16_t value,
+        uint16_t index,
+        uint16_t length,
+        uint8_t *buffer_p)
+{
+    CyU3PUsbHostEpStatus_t epStatus;
+    CyU3PReturnStatus_t status = CY_U3P_SUCCESS;
+
+    glSetupPkt[0] = type;
+    glSetupPkt[1] = request;
+    glSetupPkt[2] = CY_U3P_GET_LSB(value);
+    glSetupPkt[3] = CY_U3P_GET_MSB(value);
+    glSetupPkt[4] = CY_U3P_GET_LSB(index);
+    glSetupPkt[5] = CY_U3P_GET_MSB(index);
+    glSetupPkt[6] = CY_U3P_GET_LSB(length);
+    glSetupPkt[7] = CY_U3P_GET_MSB(length);
+
+    status = CyU3PUsbHostSendSetupRqt (glSetupPkt, buffer_p);
+    if (status != CY_U3P_SUCCESS)
+    {
+        return status;
+    }
+    status = CyU3PUsbHostEpWaitForCompletion (0, &epStatus,
+            CY_FX_HOST_EP0_WAIT_TIMEOUT);
+
+    return status;
+}
+
+/* This function initializes the mouse driver application. */
+void CyFxApplnStart ()
+{
+}
+
+/* This function disables the mouse driver application. */
+void CyFxApplnStop ()
+{
+
+}
+
+/* This function initializes the USB host stack. */
+void CyFxUsbHostStart ()
+{
+    CyU3PUsbHostConfig_t hostCfg;
+    CyU3PReturnStatus_t status;
+
+    hostCfg.ep0LowLevelControl = CyFalse;
+    hostCfg.eventCb = CyFxHostEventCb;
+    hostCfg.xferCb = NULL;
+    status = CyU3PUsbHostStart (&hostCfg);
+    if (status != CY_U3P_SUCCESS)
+    {
+        return;
+    }
+}
+
+/* This function disables the USB host stack. */
+void CyFxUsbHostStop ()
+{
+    /* Stop host mode application if running. */
+    if (glIsApplnActive)
+    {
+        CyFxApplnStop ();
+    }
+
+    /* Stop the host module. */
+    CyU3PUsbHostStop ();
+}
+
+/* This function enables / disables driving VBUS. */
+void CyFxUsbVBusControl (CyBool_t isEnable)
+{
+    if (isEnable)
+    {
+        /* Drive VBUS only if no-one else is driving. */
+        if (!CyU3POtgIsVBusValid ())
+        {
+            CyU3PGpioSimpleSetValue (21, CY_FX_HOST_VBUS_ENABLE_VALUE);
+        }
+    }
+    else
+    {
+        CyU3PGpioSimpleSetValue (21, CY_FX_HOST_VBUS_DISABLE_VALUE);
+    }
+
+    CyU3PDebugPrint (2, "VBUS enable:%d\r\n", isEnable);
+}
 
 /* OTG event handler. */
 void CyFxOtgEventCb (CyU3POtgEvent_t event,uint32_t input)
--- fw/a3/otg.h
+++ fw/a3/otg.h
@@ -4,6 +4,12 @@
 #include "cyu3types.h"
 #include "cyu3usbotg.h"
 
+#define CY_FX_HOST_VBUS_ENABLE_VALUE    (CyTrue)        /* GPIO value for driving VBUS. */
+#define CY_FX_HOST_VBUS_DISABLE_VALUE   (!CY_FX_HOST_VBUS_ENABLE_VALUE)
+#define CY_FX_USB_CHANGE_EVENT          (1 << 0)        /* Event signaling that a peripheral change is detected. */
+#define CY_FX_HOST_EP0_SETUP_SIZE       (32)            /* EP0 setup packet buffer size made multiple of 32 bytes. */
+#define CY_FX_HOST_EP0_WAIT_TIMEOUT     (5000)          /* EP0 request timeout in clock ticks. */
+
 void CyFxOtgEventCb (CyU3POtgEvent_t event,uint32_t input);
 CyU3PReturnStatus_t CyFxApplnInit (void);
 
